b13d954ece36a69dc2ef18c8b49aa0085416597d
mdb <mdb@D0CWGM2.qc.qcsoftware.us>
2018-10-12 11:55:14 -0400

    Initial commit of the stp plugin.

---
+++ src/org/billman/stp/STPTemplate.sql
@@ -0,0 +1,26 @@
+/******************************************************************************
+ *
+ * Filename:     ${nameAndExt}
+ * Description:
+ *
+ * (c) COPYRIGHT QC Software, LLC ${date?date?string("yyyy")} All Rights Reserved
+ * No part of this copyrighted work may be reproduced, modified, or distributed
+ * in any form or by any means without the prior written permission of
+ * QC Software, LLC.
+ *
+ * $Id$
+ * Notes:
+ *
+ *****************************************************************************/
+
+/******************************************************************************
+ * procedure ${name}
+ *
+ * $Id$
+ *****************************************************************************/
+create or replace procedure ${name}(<#if paramInfo == "">)<#else>
+  ${paramInfo})</#if>
+begin
+end;
+
+grant execute on ${name} to qcUsers
---
+++ src/org/billman/stp/TrgTemplate.sql
@@ -0,0 +1,27 @@
+/******************************************************************************
+ *
+ * Filename:     ${nameAndExt}
+ * Description:
+ *
+ * (c) COPYRIGHT QC Software, LLC ${date?date?string("yyyy")} All Rights Reserved
+ * No part of this copyrighted work may be reproduced, modified, or distributed
+ * in any form or by any means without the prior written permission of
+ * QC Software, LLC.
+ *
+ * $Id$
+ * Notes:
+ *
+ *****************************************************************************/
+
+/******************************************************************************
+ * trigger ${name}
+ *
+ * $Id$
+ *****************************************************************************/
+create or replace trigger ${name}
+  ${beforeOrAfter} ${updateEvent} on ${tableName}
+  referencing old as oldRec
+              new as newRec
+  for each row
+begin
+end;
