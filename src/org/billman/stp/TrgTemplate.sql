78d1c235c602d1a187a5aff6ac805e62ba118019
mdb <mdb@D0CWGM2.qc.qcsoftware.us>
2019-08-22 07:58:31 -0400

    Fixed template for new headers.

--- src/org/billman/stp/TrgTemplate.sql
+++ src/org/billman/stp/TrgTemplate.sql
@@ -1,16 +1,10 @@
-/******************************************************************************
- *
- * Filename:     ${nameAndExt}
- * Description:
- *
- * (c) COPYRIGHT QC Software, LLC ${date?date?string("yyyy")} All Rights Reserved
+/* ****************************************************************************
+ * (c) COPYRIGHT QC Software, LLC. ${date?date?string("yyyy")} All Rights Reserved
  * No part of this copyrighted work may be reproduced, modified, or distributed
  * in any form or by any means without the prior written permission of
  * QC Software, LLC.
  *
  * $Id$
- * Notes:
- *
  *****************************************************************************/
 
 /******************************************************************************
